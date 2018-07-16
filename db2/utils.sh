f111557e16928b4ede448bec2f9f96c7f8085073
Chad Scott <chads@us.ibm.com>
2018-07-16 14:09:28 -0500

    Added downloadFile utility function

--- db2/utils.sh
+++ db2/utils.sh
@@ -4,19 +4,19 @@
 # $3: User or group name
 function checkUserGroupStatus() {
 
-    local code=${1}
-    local message=${2}
-    local entity=${3}
-    local operation=${4}
+    local code="${1}"
+    local message="${2}"
+    local entity="${3}"
+    local operation="${4}"
 
-    if [[ ${code} != 0 ]]; then
+    if [[ "${code}" != 0 ]]; then
         # Non-fatal error
-        if [[ ${code} == 9 ]]; then
-            echo "W ${entity} already exists. Continuing..."	
+        if [[ "${code}" == 9 ]]; then
+            printf "W ${entity} already exists. Continuing...\n"	
         # Fatal
         else
-            echo "${message} ${entity}"
-            echo "E Exit status: ${code}"
+            printf "${message} ${entity}\n"
+            printf "E Exit status: ${code}\n"
             exit 1
         fi
     fi
@@ -28,10 +28,10 @@ function checkUserGroupStatus() {
 # $2: file name
 function downloadFile() {
 
-    local dir=${1}
-    local file=${2}
-    log "I Downloading ${file} from ${ftpServer}..."
-    ${curl} ftp://${ftpServer}/${dir}/${file}
-    checkStatus ${?} "E Download failed. Exiting."
+	 local server="${1}"
+    local dir="${2}"
+    local file="${3}"
+    printf "I Downloading ${file} from ${server}...\n"
+    ${curl} "ftp://${server}/${dir}/${file}" || printf "E Download failed. Exiting\n"
 	 
 }
