92b02ad3b811691a465b2af8ba25e951856e3ee0
Chad Scott <chads@us.ibm.com>
2018-07-18 09:17:44 -0500

    Refactoring

--- db2/createInstance.sh
+++ db2/createInstance.sh
@@ -40,6 +40,16 @@ else
 	warn "DB2 instance already exists at ${db2DataDir}/${db2InstanceUser}/${db2InstanceUser}. Skipping"
 fi
 
+# Create a new db2nodes.cfg file (needed because the image ID is there currently and will cause SQL6031N)
+printf "0 %s\n" "$(hostname)" >|"${db2DataDir}/${db2InstanceUser}/sqllib/db2nodes.cfg"
+
+# Update the /etc/services file to include the port mapping for the instance (also to prevent SQL6031N)
+printf "%s\t%s\t\t%s\n" "DB2_${db2InstanceUser}" "50000/tcp" "# DB2 instance" >>"/etc/services"
+
+# Start the DB2 instance
+inform "Starting DB2 instance..."
+su - "${db2InstanceUser}" -c "db2start"
+
 # Enable Unicode
 inform "Enabling Unicode codepage..."
 su - "${db2InstanceUser}" -c "\"${db2DataDir}/${db2InstanceUser}/sqllib/adm/db2set\" DB2CODEPAGE=1208 >/dev/null 2>&1" || \
