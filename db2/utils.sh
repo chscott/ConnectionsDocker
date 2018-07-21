314f8c825c95c028ab346a41f704bf1f545c0a58
Chad Scott <chads@us.ibm.com>
2018-07-20 21:20:57 -0500

    Changed wizards env variable

--- db2/utils.sh
+++ db2/utils.sh
@@ -7,8 +7,18 @@ function log() {
     local severity="${1}"
     local message="${2}"
     local now="$(date '+%F %T')"
-    printf "%s %s: %s\n" "${now}" "${severity}" "${message}"
-
+    
+    NORMAL_COLOR=$'\e[0m'
+    WARN_COLOR=$'\e[1;33m'
+    FAIL_COLOR=$'\e[1;31m'
+    
+    if [[ "${severity}" == "I" ]]; then
+        printf "%s %s: %s\n" "${now}" "${severity}" "${message}"
+    elif [[ "${severity}" == "W" ]]; then
+        printf "${WARN_COLOR}%s %s: %s${NORMAL_COLOR}\n" "${now}" "${severity}" "${message}"
+    elif [[ "${severity}" == "F" ]]; then
+        printf "${FAIL_COLOR}%s %s: %s${NORMAL_COLOR}\n" "${now}" "${severity}" "${message}"
+        
 }
 
 # Print an information message
@@ -30,7 +40,7 @@ function warn() {
 # Print a failure message
 function fail() {
 
-    local message="${1}"    
+    local message="${1}"  
     log "F" "${message}"
 
 }
