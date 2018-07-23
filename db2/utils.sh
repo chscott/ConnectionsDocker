bf4a74df93e37dd44db15f7c0fca13e5c2f948f1
Chad Scott <chads@us.ibm.com>
2018-07-23 13:10:11 -0500

    Working on CR1 updates

--- db2/utils.sh
+++ db2/utils.sh
@@ -250,7 +250,7 @@ function init() {
     # Update the /etc/services file to include the port mapping for the instance (also to prevent SQL6031N)
     inform "Adding DB2 instance to /etc/services file..."
     if [[ $(grep "db2inst1" "/etc/services" | grep -c 50000) > 0 ]]; then 
-        info "Entry already exists in /etc/services for DB2_db2inst1. Skipping"
+        inform "Entry already exists in /etc/services for DB2_db2inst1. Skipping"
     else
         printf "%s\t%s\t\t%s\n" "DB2_db2inst1" "50000/tcp" "# DB2 instance" >>"/etc/services" || warn "Unable to update /etc/services"
     fi
