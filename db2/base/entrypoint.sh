dee0d2369a377f190374696442f4c2d53c16437f
Chad Scott <chads@us.ibm.com>
2018-07-30 15:09:13 -0500

    Cleanup associated with documentation

---
+++ db2/base/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WORK_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# If the SETUP_URL variable has not been set, use the default URL
+if [[ -z "${SETUP_URL}" ]]; then
+    export SETUP_URL="https://raw.githubusercontent.com/chscott/ConnectionsDocker/master"
+fi
+
+# Download the run script and utilities
+curl -L -O -J -s -S -f "${SETUP_URL}/db2/base/run.sh" || printf "W: Unable to download ${SETUP_URL}/db2/base/run.sh. Trying local run.sh...\n"
+
+if [[ -f "${WORK_DIR}/run.sh" ]]; then
+    # Make the run script executable
+    chmod +x "${WORK_DIR}/run.sh"
+    # Replace this process with run.sh
+    exec "${WORK_DIR}/run.sh" || { printf "F: Error encountered trying to run run.sh. Exiting\n"; exit 1; }
+else
+    printf "F: ${WORK_DIR}/run.sh does not exist. Exiting\n"
+    exit 1
+fi
